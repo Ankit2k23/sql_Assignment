34bdd34a9db7c95bc8a1fdf3f635717caa38a2b3
Ankit2k23 <122517727+Ankit2k23@users.noreply.github.com>
2023-02-11 22:40:32 +0530

    Ques1 file added

--- QUES1.sql
+++ QUES1.sql
@@ -1,24 +1,25 @@
-# CREATING DATABASE
+--create database if not exit
+
 CREATE DATABASE IF NOT EXISTS Company;
 USE Company;
-
-
-#Creating table employee
-CREATE TABLE Employees(
-   emp_id INT PRIMARY KEY AUTO_INCREMENT,
-   emp_name VARCHAR(30) NOT NULL,
-   gender VARCHAR(30) NOT NULL,
-   department VARCHAR(30) NOT NULL
+--creating table
+CREATE TABLE Employee(
+   Emp_id INT PRIMARY KEY AUTO_INCREMENT,
+   Emp_name VARCHAR(25) NOT NULL,
+   Gender VARCHAR(25) NOT NULL,
+   department VARCHAR(25) NOT NULL
 );
-#Inserting values
-INSERT INTO employees(emp_name,gender,department)
+
+--Inserting values in employee table
+
+INSERT INTO employee(Emp_name,Gender,department)
 VALUES('X','Female','Finance'),
       ('Y','Male','IT'),
       ('Z','Male','HR'),
       ('W','Female','IT');
       
-SELECT * FROM employees;
-#Query
+SELECT * FROM employee;
+--Query
 SELECT department AS Department,
        SUM(CASE
                WHEN gender='Male' THEN 1
@@ -28,5 +29,6 @@ SELECT department AS Department,
                WHEN gender='Female' THEN 1
                ELSE 0
            END) AS 'Count of Female'  
-FROM employees
+FROM employee
 GROUP BY department;
+
